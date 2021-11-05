4ad6b41030f50d57b690c2c7e7738983480cab86
Evhenii Bondarenko <alessanjeka@gmail.com>
2021-11-06 00:37:17 +0200

    0.3

--- hw1/src/main/java/org/geekhub/evhenii/phoneCalc.java
+++ hw1/src/main/java/org/geekhub/evhenii/phoneCalc.java
@@ -5,5 +5,7 @@ public class phoneCalc {
     public static void main(String[] args) {
 
 
+
     }
 }
+
--- hw1/src/main/java/org/geekhub/evhenii/phoneInput.java
+++ hw1/src/main/java/org/geekhub/evhenii/phoneInput.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 public class phoneInput {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Please, enter your phone number, start from country code");
-    long phoneNumber = sc.nextLong();
-    String phoneString = String.valueOf(phoneNumber);
-    if (phoneString.length() != 12){
-        System.out.println("Please, provide correct phone number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter your phone number, start from country code e.g. 380");
+
+        long phoneNumber = sc.nextLong();
+        String phoneString = String.valueOf(phoneNumber);
+
+        if (phoneString.length() != 12) {
+            System.out.println("Please, provide correct phone number");
+        } else {
+            System.out.println("GJ first step complete " + phoneNumber);
+        }
+
+            int sum = 0;
+            char[] pA = phoneString.toCharArray();
+            for (int i = 0; i < pA.length; i++) {
+                sum = sum + Character.getNumericValue(pA[i]);
+
+            }
+
+    System.out.println("StringLength " + phoneString.length() + " SUM = " + sum + " Phone = "+ phoneString);
+
     }
-    else {
-        System.out.println("GJ first step complete " + phoneNumber);
-    }
-    }
+
 }
