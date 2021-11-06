51141c9bc90296ed2c04ee7444fb4753143ff743
Evhenii Bondarenko <alessanjeka@gmail.com>
2021-11-06 04:53:52 +0200

    0.6

--- hw1/src/main/java/org/geekhub/evhenii/phoneInput.java
+++ hw1/src/main/java/org/geekhub/evhenii/phoneInput.java
@@ -8,22 +8,51 @@ public class phoneInput {
         System.out.println("Please, enter your phone number, start from country code e.g. 380");
 
         long phoneNumber = sc.nextLong();
+        String question = "";
         String phoneString = String.valueOf(phoneNumber);
 
-        if (phoneString.length() != 12) {
-            System.out.println("Please, provide correct phone number");
-        } else {
-            System.out.println("GJ first step complete " + phoneNumber);
+        if (phoneString.length() != 12 & phoneString.substring(3).equals("380")) {
+            while (phoneString.length() != 12) {
+                System.out.println("Please, provide correct phone number");
+                phoneString = sc.nextLine();
+            }
         }
 
-            int sum = 0;
-            char[] pA = phoneString.toCharArray();
-            for (int i = 0; i < pA.length; i++) {
-                sum = sum + Character.getNumericValue(pA[i]);
+        else if (phoneString.length() == 12 | phoneString.substring(3).equals("380")) {
+            System.out.println("GJ! Thanks a lot " + phoneNumber);
+        }
 
-            }
+        int sum = 0;
+        char[] pA = phoneString.toCharArray();
+        for (int i = 0; i < pA.length; i++) {
+            sum += Character.getNumericValue(pA[i]);
 
-    System.out.println("StringLength " + phoneString.length() + " SUM = " + sum + " Phone = "+ phoneString);
+        }
+        int hwSum = 0;
+        char[] sum2 = String.valueOf(sum).toCharArray();
+        for (int i = 0; i < sum2.length; i++) {
+            hwSum += Character.getNumericValue(sum2[i]);
+        }
+
+        switch (hwSum) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            default:
+                System.out.println(hwSum);
+        }
+
+
+        System.out.println(" Phone = "+ phoneString +"; StringLength " + phoneString.length() + "; SUM after 1 step = " + sum + "; Sum after last step " + hwSum);
 
     }
 
